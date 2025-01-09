0c8c6cc82167a9b717a448ad5e7de9236dc51d31
Sagar Jain <75481949+sagarkjain@users.noreply.github.com>
2025-01-09 19:06:24 +0530

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      specPattern: "cypress/e2e/**/*.cy.js"
     },
   },
 });
