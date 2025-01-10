da3315d49b8e6f0ea3e9165013b798714eac8b4c
sagarkjain <jain.ksagar@gmail.com>
2025-01-10 11:16:51 +0530

    Add test report plugin

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,15 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
       specPattern: "cypress/e2e/**/*.cy.js"
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
+    reporter: "cypress-mochawesome-reporter",
+    reporterOptions: {
+      charts: true,
+      reportPageTitle: "Cypress Test Report",
+      embeddedScreenshots: true,
+      inlineAssets: true,
+      saveAllAttempts: false,
+    }
   },
 });
